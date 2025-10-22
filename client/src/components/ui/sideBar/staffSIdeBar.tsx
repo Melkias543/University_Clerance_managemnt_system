e681831fd6110c5cff01aaa902eedfe9e3f78574
ABDIFEKEDA <abdifk23@gmail.com.com>
2025-10-22 23:37:28 +0300

    add issued student popup

--- client/src/components/ui/sideBar/staffSIdeBar.tsx
+++ client/src/components/ui/sideBar/staffSIdeBar.tsx
@@ -7,6 +7,7 @@ import { useAuth } from "@/context/authContext";
 
 const StaffSideBar = () => {
   const [active, setActive] = useState("Dashboard");
+   const [isOpen, setIsOpen] = useState(false); 
 const {logout} = useAuth()
   const handleLogout = () => {
   logout();
@@ -65,6 +66,7 @@ const {logout} = useAuth()
             </Link>
           ))}
         </ul>
+        
       </div>
     </aside>
   );
